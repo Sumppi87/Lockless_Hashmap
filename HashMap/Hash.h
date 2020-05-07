344df7589195e6fd4f4adf3ad1aa76569b619a3e
Juha Löflund <juha.loflund@wapice.com>
2020-05-07 23:12:52 +0300

    Added basic internal members

--- HashMap/Hash.h
+++ HashMap/Hash.h
@@ -1,11 +1,34 @@
 #pragma once
 #include <atomic>
+#include <intrin.h>
 
-template<typename K, typename V, size_t size>
+#pragma intrinsic(_BitScanReverse)
+
+template<typename K, typename V, size_t MAX_ELEMENTS>
 class Hash
 {
-public:
+private:
+	constexpr static size_t ComputeHashKeyCount()
+	{
+		size_t v = MAX_ELEMENTS * 2;
 
+		v--;
+		v |= v >> 1;
+		v |= v >> 2;
+		v |= v >> 4;
+		v |= v >> 8;
+		v |= v >> 16;
+		v++;
+		return v;
+	}
+	constexpr static const size_t KEY_COUNT = ComputeHashKeyCount();
+	constexpr static const size_t MASK = KEY_COUNT - 1;
+public:
+	Hash() :
+		m_hash{nullptr}
+	{
+
+	}
 
 
 
@@ -15,13 +38,23 @@ private:
 	class HashKeyT
 	{
 	public:
+		HashKeyT() :
+			k(),
+			v(),
+			hash(),
+			recycled(true),
+			accessing(false)
+		{
+
+		}
 		K k;
 		V v;
 		size_t hash;
 		std::atomic_bool recycled;
-		std::atomic_bool access_ongoing;
+		std::atomic_bool accessing;
 	};
 	typedef HashKeyT<K, V>  HashKey;
 
-	HashKey m_keys[size];
+	std::atomic<HashKey*> m_hash[MAX_ELEMENTS];
+	HashKey m_recycle[KEY_COUNT];
 };
